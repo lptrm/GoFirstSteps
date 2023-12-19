ea59a8d70e62d56559c046ffcd1d72506e57e315
jobernberg <jobernberg@stud.hs-heilbronn.de>
2023-12-19 19:50:17 +0100

    solved day two part 2

---
+++ advent-of-code/day02/part2/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sum := 0
+	regexPattern := `\b\d{1,2}\s\w+\b`
+	reader := bufio.NewReader(os.Stdin)
+
+	gameCounter := 0
+
+	for {
+		greenCubes := 0
+		redCubes := 0
+		blueCubes := 0
+		line, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			return
+		} else {
+			if strings.TrimSpace(line) == "end" {
+				break
+			}
+			if strings.TrimSpace(line) == "" {
+				continue
+			}
+			gameCounter += 1
+			regex, err := regexp.Compile(regexPattern)
+			if err != nil {
+				fmt.Println("Error compiling regex:", err)
+				return
+			}
+			if err != nil {
+				fmt.Println("Error compiling regex:", err)
+				return
+			}
+			cubeSet := strings.Split(line, ";")
+			for _, cube := range cubeSet {
+				matches := regex.FindAllString(cube, -1)
+				for _, match := range matches {
+					number, err := strconv.Atoi(strings.Split(match, " ")[0])
+					if err != nil {
+						fmt.Println("Error converting string to int:", err)
+						return
+					}
+					color := strings.Split(match, " ")[1]
+					switch color {
+					case "green":
+						if number > greenCubes {
+							greenCubes = number
+						}
+					case "red":
+						if number > redCubes {
+							redCubes = number
+						}
+					case "blue":
+						if number > blueCubes {
+							blueCubes = number
+						}
+					}
+				}
+
+			}
+			sum += greenCubes * redCubes * blueCubes
+		}
+	}
+	fmt.Println("Sum:", sum)
+}
