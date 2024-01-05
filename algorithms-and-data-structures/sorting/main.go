dd3dff616947d05333ecf802efa9f0d895559d93
jobernberg <jobernberg@stud.hs-heilbronn.de>
2024-01-05 19:24:22 +0100

    did further sorting algorithms

--- algorithms-and-data-structures/sorting/main.go
+++ algorithms-and-data-structures/sorting/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	originalString := "thequickbrownfoxjumpsoverthelazydog"
@@ -9,9 +12,38 @@ func main() {
 
 	fmt.Println("Byte Array:", byteArray)
 	fmt.Println("String Rep:", string(byteArray))
+
+	fmt.Println("selection sort")
+	startTime := time.Now()
 	selectionSort(byteArray)
+	endTime := time.Now()
 	fmt.Println("Sorted Array:", byteArray)
 	fmt.Println("String Rep:", string(byteArray))
+	fmt.Println("The Time was:", endTime.Sub(startTime))
+
+	fmt.Println("bubbleSort")
+	startTime = time.Now()
+	bubbleSort(byteArray)
+	endTime = time.Now()
+	fmt.Println("Sorted Array:", byteArray)
+	fmt.Println("String Rep:", string(byteArray))
+	fmt.Println("The Time was:", endTime.Sub(startTime))
+
+	fmt.Println("insertionSort")
+	startTime = time.Now()
+	insertionSort(byteArray)
+	endTime = time.Now()
+	fmt.Println("Sorted Array:", byteArray)
+	fmt.Println("String Rep:", string(byteArray))
+	fmt.Println("The Time was:", endTime.Sub(startTime))
+
+	fmt.Println("shellSort")
+	startTime = time.Now()
+	shellSort(byteArray)
+	endTime = time.Now()
+	fmt.Println("Sorted Array:", byteArray)
+	fmt.Println("String Rep:", string(byteArray))
+	fmt.Println("The Time was:", endTime.Sub(startTime))
 
 }
 func selectionSort(s []byte) {
@@ -28,7 +60,47 @@ func selectionSort(s []byte) {
 		swap(s, i, index)
 	}
 }
+func selectionSortRec(s []byte, i int, j int, index int, current byte) {
 
+}
+func bubbleSort(s []byte) {
+	sorted := false
+	for !sorted {
+		sorted = true
+		for i := 0; i < len(s)-1; i++ {
+			if s[i] > s[i+1] {
+				sorted = false
+				swap(s, i, i+1)
+			}
+		}
+	}
+}
+func insertionSort(s []byte) {
+	for i := 0; i < len(s); i++ {
+		for j := i; j > 0 && s[j] < s[j-1]; {
+			swap(s, j, j-1)
+		}
+	}
+}
+
+func shellSort(s []byte) {
+	hSeries := [7]int{1093, 364, 121, 40, 13, 4, 1}
+	for _, h := range hSeries {
+		if h > len(s) {
+			continue
+		}
+		insertionSortH(s, h)
+	}
+
+}
+
+func insertionSortH(s []byte, h int) {
+	for i := 0; i < len(s); i = i + h {
+		for j := i; j > 0 && s[j] < s[j-h]; j = j - h {
+			swap(s, j, j-h)
+		}
+	}
+}
 func swap(s []byte, i int, index int) {
 	if i != index {
 		tmp := s[i]
